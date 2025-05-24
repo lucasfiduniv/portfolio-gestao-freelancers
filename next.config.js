5e8584c456a1932783256c77f5cab74aad122d32
Gabriela Sena <lucasgabriel22222222@gmail.com>
2025-05-24 17:05:19 -0300

    fix: 🐛 corrigindo erro

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Gera arquivos HTML estáticos (Next.js Export Mode)
-  output: 'export',
-
-  // Ignora erros do ESLint no build
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  // Usa <Image> sem otimização, necessário com export
-  images: {
-    unoptimized: true,
-  },
-
-  // Habilita compatibilidade com imagens do public/
-  trailingSlash: true, // Opcional: evita 404 em alguns servidores estáticos
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
